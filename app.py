9f8611b0e9234e5b06d5b1e1af2893e8383bbb16
qiao-yu100 <qiaoyuyou@micron.com>
2025-08-06 10:08:43 +0800

    app.py

--- app.py
+++ app.py
@@ -28,11 +28,39 @@ summary_df['Scrap wafer'] = scrap_values
 
 # 計算 Weekly NCD% prediction
 total_shipped_die_sum = summary_df['Total_shipped_die'].sum()/2
-summary_df['Weekly NCD% prediction'] = summary_df['DPW'] * summary_df['Scrap wafer'] / total_shipped_die_sum
+# 計算百分比並格式化
+summary_df['Weekly NCD% prediction'] = (
+    summary_df['DPW'] * summary_df['Scrap wafer'] / total_shipped_die_sum
+) * 100
+
+# 四捨五入並加上 % 字串
+summary_df['Weekly NCD% prediction'] = summary_df['Weekly NCD% prediction'].round(2).astype(str) + '%'
+
+weekly_ncd_sum = weekly_ncd_raw.sum().round(2)
+
+# 建立 sum row
+sum_row = {
+    'DID': 'sum',
+    'DPW': summary_df['DPW'].sum(),
+    'GOOD_DIE': summary_df['GOOD_DIE'].sum(),
+    'CQDR_DIE': summary_df['CQDR_DIE'].sum(),
+    'Total_shipped_die': summary_df['Total_shipped_die'].sum(),
+    'SQDR_DIE': summary_df['SQDR_DIE'].sum(),
+     'SQDR NCD Percent': f"{(summary_df['SQDR_DIE'].sum() / summary_df['Total_shipped_die'].sum()) * 100:.2f}%",
+    'GOOD_WAFER': summary_df['GOOD_WAFER'].sum(),
+    'CQDR_WAFER': summary_df['CQDR_WAFER'].sum(),
+    'Scrap wafer': summary_df['Scrap wafer'].sum(),
+    'Weekly NCD% prediction': f"{weekly_ncd_sum}%"  # 加上百分比字串
+}
+
+# 加入 sum row 到 DataFrame
+summary_df = pd.concat([summary_df, pd.DataFrame([sum_row])], ignore_index=True)
+
 
 # 顯示更新後的資料表
 st.subheader("更新後的資料表")
 st.dataframe(summary_df)
+
 
 
 
