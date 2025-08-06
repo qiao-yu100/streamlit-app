216209b4740610fdd20899ffac794300966d0b67
qiao-yu100 <qiaoyuyou@micron.com>
2025-08-06 09:45:40 +0800

    app.py

--- app.py
+++ app.py
@@ -13,7 +13,9 @@ st.dataframe(summary_df)
 st.subheader("請輸入每一列的 Scrap wafer 數量")
 scrap_values = []
 
-for i in range(len(summary_df)-1):
+count_value = len(summary_df)-1
+
+for i in range(count_value):
     scrap = st.number_input(
         label=f"{summary_df.at[i, 'DID']} 的 Scrap wafer",
         min_value=0.0,
@@ -33,6 +35,7 @@ summary_df['Weekly NCD% prediction'] = summary_df['DPW'] * summary_df['Scrap waf
 # 顯示更新後的資料表
 st.subheader("更新後的資料表")
 st.dataframe(summary_df)
+
 
 
 
